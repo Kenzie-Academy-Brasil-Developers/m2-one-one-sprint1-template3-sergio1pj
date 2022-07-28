19c5ba4a14ce0e4e0237ed5079a80fca8a5ec462
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-28 12:40:48 -0300

    versão 1

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ const listaDeDevs = [
   },
 ];
 
+
+
+let main = document.querySelector("#main__container");
+let footer = document.querySelector("#main__footer");
+
+function createCard(element) {
+  let tagDiv = document.createElement("div");
+  let tagH2 = document.createElement("h2");
+  let tagImg = document.createElement("img");
+  tagH2.innerText = element.nome;
+    tagImg.src = element.image;
+    if(element.stackDeEstudo == "front-end") {
+      tagDiv.classList.add("dev__frontend")
+    } else if(element.stackDeEstudo == "back-end") {
+      tagDiv.classList.add("dev__backend");
+    }
+  tagDiv.appendChild(tagImg);
+  tagDiv.appendChild(tagH2);
+  return tagDiv
+}
+
+function createSection(array) {
+  let sectionDevsList = document.querySelector("#devs__list");
+  array.forEach((element) => {
+    sectionDevsList.appendChild(createCard(element))
+  });
+}
+
+createSection(listaDeDevs);
+footer.remove();
+
+
